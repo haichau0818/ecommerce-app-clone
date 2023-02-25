e7a313612d0abda10db9c270041ad308c21e139f
unknown <haichau0818@gmail.com>
2023-02-25 13:09:24 +0700

    create components by hei

---
+++ ecomerce-clone-frontent/src/components/RouterPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter as Router,RouterProps,Route} from 'react-router-dom'
+
+export default function RouterPage(){
+    return(
+        
+        <Router>
+            <Routers>
+                {/* <Route path='/' element{}/>
+
+                </Route> */}
+            </Routers>
+        </Router>
+
+
+        )
+}
